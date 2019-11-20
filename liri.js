8fa931dc2d5a392c850b5a1f5c9e524064c1d552
ruok27 <jakxii27@gmail.com>
2019-11-19 19:10:31 -0500

    Spotify track data is now able to be displayed

--- liri.js
+++ liri.js
@@ -1,9 +1,29 @@
-let env = require("dotenv").config();
+const env = require("dotenv").config();
+const Spotify = require('node-spotify-api');
+const axios = require("axios");
+const fileSystem = require("fs");
+const keys = require("./keys.js");
 
-let fileSystem = require("fs");
-var keys = require("./keys.js");
+
+
+
+
+
+
+
 let spotify = new Spotify(keys.spotify);
 
+
+spotify.search({
+  type: 'track',
+  query: 'The Sign Ace of Base'
+ }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data.tracks.items[1]); 
+});
 
 
 //TODO
@@ -19,7 +39,7 @@ let spotify = new Spotify(keys.spotify);
 // do-what-it-says use fs to make the content of random.txt the fourth argument
 
 
-const axios = require("axios");
+
 
 
 axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
